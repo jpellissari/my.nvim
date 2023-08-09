ef8fb06f342dbcf88d0e95118e01fc8c0473813c
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2023-08-09 20:37:26 -0300

    feat: new keybindings for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -27,7 +27,8 @@ telescope.setup({
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Search Files" })
 vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Search Files" })
--- vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Search Git Files " })
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Search live grep" })
+vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "Search buffers" })
 vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [h]elp pages" })
 vim.keymap.set("n", "<leader>sw", function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") });
